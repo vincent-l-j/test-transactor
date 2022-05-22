278ff06c954651e2499854637a393782e9b97ffe
Vincent Jadraque <vincent.jadraque@gmail.com>
2022-05-22 14:32:13 +1000

    Add an empty toolbar using material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Toolbar } from "@material-ui/core";
+import {
+  createTheme,
+  ThemeProvider,
+  makeStyles,
+} from "@material-ui/core/styles";
+import { lightBlue, blueGrey } from "@material-ui/core/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blueGrey[300],
+    },
+    secondary: {
+      main: lightBlue[600],
+    },
+  },
+});
+
+const useStyles = makeStyles({
+  root: {
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh",
+  },
+  appBar: {
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <AppBar className={classes.appBar} position="static">
+          <Toolbar>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </ThemeProvider>
   );
 }
 
